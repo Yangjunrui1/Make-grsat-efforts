95c6f572b89b9211a8cd3ab574f76cef3ac77894
Yangjunrui1 <1550172033@qq.com>
2024-11-15 22:33:35 +0800

    10月15日练习

---
+++ test11_15,2.c
@@ -0,0 +1,22 @@
+
+
+
+#include<stdio.h>
+int main(void)
+{
+	void num;
+	num = 1;
+	printf("I am a simble ");
+	printf("computer.\n");
+	printf("My favorite number is %d because it is first.\n ", num);
+
+
+
+
+
+
+
+	
+
+
+}
