746d755e693d20bf5c7f5e377ed84be87d4bded3
Yangjunrui1 <1550172033@qq.com>
2024-11-16 09:58:37 +0800

    输入三个整数运用任意四则运算组合求出最大值

---
+++ add.c
@@ -0,0 +1,65 @@
+#define _CRT_SECURE_NO_WARNINGS
+//#include<stdio.h>
+//int main()
+//{
+//	int n = 0;
+//	scanf_s("%d", &n);
+//	int c = n /3 ;
+//	printf("%d\n", c);
+//	
+//	return 0;
+//}
+//#include<stdio.h>
+//int main()
+//{
+//	int a, b, c,mid;
+//	scanf("%d%d%d", &a, &b, &c);
+//	int z = (a > b ? a : b);
+//	int x = (z > c ? z : b);
+//	int m = (a < b ? a : b);
+//	int r = (m < c ? m : c);
+//	if (a >= b && b <= c)
+//	{
+//		mid = b;
+//	}
+//	if (c >= a && a <= b)
+//	{
+//		mid = a;
+//	}
+//	if (a >= c && c <= b)
+//	{
+//		mid = c;
+//	}
+//
+//	int max = x*(r+mid);
+//	printf("%d\n", max);
+//
+//	 
+//	
+//	return 0;
+//
+//}
+#include<stdio.h>
+int slove(int x, int y, int z)
+{
+	int ret[] = { x + y + z,(x + y) * z,(x + z) * y,(z + y) * x,x * y * z,x + y * z,z + x * y,y + z * x };
+	int turn = ret[0];
+	for (int i=1; i < 8; i++)
+	{
+		if (ret[i] >= turn)
+		{
+			turn = ret[i];
+
+		}
+
+	}
+	return turn;
+}
+int main()
+{
+	int num1, num2, num3;
+	scanf("%d%d%d", &num1, &num2, &num3 );
+	int max = slove(num1, num2, num3);
+	printf("%d\n", max);
+	return 0;
+}
