51441885177e74afbc1cc8f6102e93328f8eea9c
Yangjunrui1 <1550172033@qq.com>
2025-01-05 19:46:19 +0800

    1月5日练习

---
+++ 链表/链表/test.c
@@ -0,0 +1,25 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+struct Node
+{
+	int date;
+	struct Node* next;
+};
+int main()
+{
+	/*struct Node n1 = { 1, NULL };
+	struct Node n2 = { 3, NULL };
+	struct Node n3 = { 3, NULL };
+	n1.next = &n2;
+	n2.next = &n3;*/
+	//简单的静态链表
+	
+	//动态创建一个链表：动态内存申情+模块化设计
+	//创建一个表头（表示整个链表）
+	//创建结点
+	//插入结点
+	// 删除结点
+	// 打印遍历链表	
+
+	return 0;
+}
