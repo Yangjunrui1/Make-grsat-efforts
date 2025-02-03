607628d9e24db670698aaafacc84d213f5b28694
Yangjunrui1 <1550172033@qq.com>
2025-02-03 18:20:39 +0800

    2月3日练习

---
+++ c++练习/c++练习/test.cpp
@@ -0,0 +1,20 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include<iostream>
+#include<string>
+using namespace std;
+int main()
+{
+	string a = "dfdsfd";
+	getline(cin, a);
+	cout << a<<"\n";
+	cout << a.length() << "\n";
+	/*bool flag = true;
+	bool flag1 = 2;
+	bool flag2 = 0;
+	std::cout << flag << flag1 << flag2;
+	int n;
+	std::cin >> n;
+	std::cout << "steetes" << ++n<<"\n";
+	*/
+	return 0;
+}
