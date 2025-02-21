00619577be3531073b25abfe6c4d0ae7af04df13
Yangjunrui1 <1550172033@qq.com>
2025-02-21 21:39:07 +0800

    2月21日练习

---
+++ Stl_list/Stl_list/test.cpp
@@ -0,0 +1,31 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+	int n, m;
+	cin >> n >> m;
+	list<int>node;//创建一个链表
+	for (int i = 1; i <= n; i++)
+	{
+		node.push_back(i);//在链表的尾部插入元素
+	}
+	list<int>::iterator it = node.begin();
+	while (node.size() > 1)//list的大小由stl自己管理
+	{
+		for (int i = 1; i < m; i++)
+		{
+			it++;
+			if (it == node.end())
+				it = node.begin();
+		}
+		cout << *it << " ";
+		list<int>::iterator next = ++it;
+		if (next == node.end())
+			next = node.begin();
+		node.erase(--it);
+			it = next;
+	}
+	cout << * it;
+	return 0;
+}
