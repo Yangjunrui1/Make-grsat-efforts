1e94599fa29e32dd325a32cb38eb511616333596
Yangjunrui1 <1550172033@qq.com>
2025-03-06 21:13:54 +0800

    3月6日练习

---
+++ 洛谷P1102/洛谷P1102/test.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+	int n, c,d;
+	cin >> n >> c;
+	vector<int>h;
+	for (int i = 0; i < n; i++)
+	{
+		cin >> d;
+		h.push_back(d);
+	}
+	int i = 1, j = 1;
+	sort(h);
+	return 0;
+}
