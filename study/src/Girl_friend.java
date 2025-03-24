e789dafa97de434e60a7607339b7e94bcf8b619d
Yangjunrui1 <1550172033@qq.com>
2025-03-24 21:23:02 +0800

    3月24日练习

---
+++ study/src/Girl_friend.java
@@ -0,0 +1,14 @@
+public class Girl_friend {
+    //属性
+    String name;
+    int age;
+    String sex;
+    //行为
+    public void eat(){
+        System.out.println("女朋友吃饭");
+    }
+    public void  sleep(){
+        System.out.println("女朋友睡觉");
+    }
+
+}
---
+++ study/src/Girl_friend_test.java
@@ -0,0 +1,12 @@
+public class Girl_friend_test {
+    public static void main(String[] args) {
+        Girl_friend gf = new Girl_friend();
+        gf.name ="小美女";
+        gf.age = 18;
+        gf.sex = "nv";
+        gf.eat();
+        gf.sleep();
+        System.out.println(gf.name);
+        System.out.println(gf.age);
+    }
+}
