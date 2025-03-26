3cd1aff9af290c73112ce7d68c7c559a5ce443b1
Yangjunrui1 <1550172033@qq.com>
2025-03-26 21:42:52 +0800

    3月26日提交

--- study/src/Girl_friend.java
+++ study/src/Girl_friend.java
@@ -1,14 +1,31 @@
 public class Girl_friend {
     //属性
-    String name;
-    int age;
-    String sex;
-    //行为
-    public void eat(){
-        System.out.println("女朋友吃饭");
-    }
-    public void  sleep(){
-        System.out.println("女朋友睡觉");
-    }
+  private int age;
+  private String name;
+  private String sex;
+  //作用给对象的属性赋值
+  public void setName(String n){
+      name = n;
+  }
+  //作用是对外提供属性
+  public String getName(){
+      return name;
+  }
+  public void setAge(int a){
+      if(a>=18&&a<=30){
+          age = a;
+      }else{
+          System.out.println("年龄太大了");
+      }
+  }
+  public int getAge(){
+      return age;
+  }
+public void setSex(String s){
+      sex = s;
+}
 
+   public String getSex(){
+      return sex;
+   }
 }
--- study/src/Girl_friend_test.java
+++ study/src/Girl_friend_test.java
@@ -1,12 +1,14 @@
+import java.util.Scanner;
 public class Girl_friend_test {
     public static void main(String[] args) {
+     //创建一个新对象
         Girl_friend gf = new Girl_friend();
-        gf.name ="小美女";
-        gf.age = 18;
-        gf.sex = "nv";
-        gf.eat();
-        gf.sleep();
-        System.out.println(gf.name);
-        System.out.println(gf.age);
+        gf.setName("Lili");
+        gf.setAge(19);
+        gf.setSex("女");
+        System.out.println(gf.getName());
+        System.out.println(gf.getAge());
+        System.out.println(gf.getSex());
+
     }
 }
