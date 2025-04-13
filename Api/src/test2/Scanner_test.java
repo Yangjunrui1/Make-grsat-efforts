67445e801d979442d848b20c1b48bf5cf7b9b6af
Yangjunrui1 <1550172033@qq.com>
2025-04-13 20:03:56 +0800

    4月14日练习

---
+++ Api/src/test2/Scanner_test.java
@@ -0,0 +1,12 @@
+package test2;
+
+import java.util.Scanner;
+
+public class Scanner_test {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        double a = sc.nextDouble();
+        System.out.println(a);
+    }
+
+}
