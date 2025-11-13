fa9b35bc36c3c9877c2a69143813670a3ca9b07e
Yangjunrui1 <1550172033@qq.com>
2025-11-13 21:50:52 +0800

    11月13日练习

--- Study_java/src/Main.java
+++ Study_java/src/Main.java
@@ -1,5 +1,9 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
+        // This is a simple Java program that prints "Hello, World!" to the console.
+        // You can run this program in any Java development environment.
+
     }
 }
+
