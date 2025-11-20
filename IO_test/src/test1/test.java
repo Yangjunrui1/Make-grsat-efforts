7175d7e0aeb417f8665e2b9c363f149131fac4ec
Yangjunrui1 <1550172033@qq.com>
2025-11-20 23:16:19 +0800

    11月20日练习

--- IO_test/src/test1/test.java
+++ IO_test/src/test1/test.java
@@ -6,9 +6,13 @@ import java.io.IOException;
 import java.lang.classfile.FieldTransform;
 public class test {
     public static void main(String[] args) throws IOException {// throws FileNotFoundException, IOException作用域在main方法中，抛出异常
-        FileOutputStream fos = new FileOutputStream("aa.txt");
+        FileOutputStream fos = new FileOutputStream("aa.txt",true);// true表示追加
         fos.write(98);
         fos.write(65);
+        fos.write(66);
+        String str = "\r\n";
+        byte[] bytes2 = str.getBytes();//
+        fos.write(bytes2);
         byte[] bytes = {97, 98, 99, 100, 101};
         fos.write(bytes);
         fos.write(bytes, 0, 2);
