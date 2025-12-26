c626b404d6a8635c960c0dd566820fac4f4eda4e
Yangjunrui1 <1550172033@qq.com>
2025-12-26 22:32:01 +0800

    12/26

--- untitled2/src/test.java
+++ untitled2/src/test.java
@@ -1,5 +1,9 @@
+import java.sql.SQLOutput;
+
 public class test {
     public static void main(String[] args) {
         System.out.println("hello world");
+        System.out.println("hello world");
+        
     }
 }
