4921e7b853d7ae154765fe6ab5c29a14fe3b03cb
Yangjunrui1 <1550172033@qq.com>
2026-03-18 17:12:11 +0800

    Update Main.java

--- lanqiaobei_prepare/src/Main.java
+++ lanqiaobei_prepare/src/Main.java
@@ -1,14 +1,14 @@
+
+
 public class Main {
     public static void main(String[] args) {
-        long n = 10000;
-        while(true){
-            if((n + 20250412) % 20240413 == 0){
-                if ((n + 20240413) % 20250412 == 0) {
-                    System.out.println(n);
-                    break;
-                }
+        long  count = 0;
+        while (true) {
+            count+=20240413;
+            if((count+20240413)%20250412==0){
+                System.out.println(count);
+                break;
             }
-            n += 20250412;
         }
     }
 }
